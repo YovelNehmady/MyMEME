9a6b0253309d49ab7768b82d79e56e708681017a
YovelNehmady <yovelnehmady1998@gmail.com>
2022-12-15 19:06:23 +0200

    creating basis function at the editor screen

--- js/meme-controller,.js
+++ js/meme-controller,.js
@@ -3,26 +3,51 @@ const gElCanvas = document.querySelector('.main-canvas')
 const gCtx = gElCanvas.getContext('2d')
 const elContainer = document.querySelector('.canvas-container')
 
-function renderMeme() {
+function renderMeme(lineIdx = 0) {
     const meme = getMeme()
     const img = getImgById(meme.selectedImgId)
     const elImg = new Image()
     elImg.src = img.url
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
-        drawText(meme)
+        drawText(meme, lineIdx)
+        drawText1(meme)
     }
 }
 
 
-function drawText(meme) {
+function drawText(meme, lineIdx) {
     gCtx.lineWidth = 2
     gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = 'white'
-    gCtx.font = "60px impact";
+    gCtx.fillStyle = meme.lines[lineIdx].color
+    gCtx.font = `${meme.lines[lineIdx].size}px impact`
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'top'
 
-    gCtx.fillText(meme.lines[0].txt, gElCanvas.width / 2, 0)
-    gCtx.strokeText(meme.lines[0].txt, gElCanvas.width / 2, 0)
+    gCtx.fillText(meme.lines[lineIdx].txt, gElCanvas.width / 2, 0)
+    gCtx.strokeText(meme.lines[lineIdx].txt, gElCanvas.width / 2, 0)
+}
+
+// function drawText(meme) {
+//     gCtx.lineWidth = 2
+//     gCtx.strokeStyle = 'black'
+//     gCtx.fillStyle = meme.lines[0].color
+//     gCtx.font = `${meme.lines[0].size}px impact`
+//     gCtx.textAlign = 'center'
+//     gCtx.textBaseline = 'top'
+
+//     gCtx.fillText(meme.lines[0].txt, gElCanvas.width / 2, 0)
+//     gCtx.strokeText(meme.lines[0].txt, gElCanvas.width / 2, 0)
+// }
+
+function drawText1(meme) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = meme.lines[1].color
+    gCtx.font = `${meme.lines[1].size}px impact`
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'top'
+
+    gCtx.fillText(meme.lines[1].txt, gElCanvas.width / 2, gElCanvas.height-70)
+    gCtx.strokeText(meme.lines[1].txt, gElCanvas.width / 2, gElCanvas.height-70)
 }
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,28 +1,63 @@
 'usr strict'
 
-var gMeme = {
+let gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
     lines: [
         {
             txt: '',
-            size: 20,
+            size: 60,
             align: 'left',
-            color: 'black'
+            color: '#ffffff'
+        },
+        {
+            txt: 'second line',
+            size: 60,
+            align: 'left',
+            color: '#ffffff'
         }
     ]
 }
 
-function setLineTxt(text){
-    gMeme.lines[0].txt = text
+function setLineTxt(text) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = text
+    renderMeme()
+}
+
+function onChangeLineFocus() {
+    const lineIdx = getLineIdx()
+    renderInputs(lineIdx)
+}
+
+function renderInputs(lineIdx) {
+    document.querySelector('.meme-input').value = gMeme.lines[lineIdx].txt
+    document.querySelector('.color-picker').value = gMeme.lines[lineIdx].color 
+}
+
+function getLineIdx() {
+    gMeme.selectedLineIdx++
+    if (gMeme.selectedLineIdx === gMeme.lines.length) {
+        gMeme.selectedLineIdx = 0
+        return gMeme.selectedLineIdx
+    }
+    return gMeme.selectedLineIdx
+}
+
+function onSetColor(color) {
+    gMeme.lines[gMeme.selectedLineIdx].color = color
+    renderMeme()
+}
+
+function onChangeFontSize(diff) {
+    gMeme.lines[gMeme.selectedLineIdx].size += diff
     renderMeme()
 }
 
 function setImg(id) {
-  gMeme.selectedImgId = id
+    gMeme.selectedImgId = id
 }
 
-function getMeme(){
+function getMeme() {
     return gMeme
 }
 
